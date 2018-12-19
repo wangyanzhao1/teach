d4035246316fb6bb7f889313a2536fccf7789e45
朕很萌 <2664808769@qq.com>
2018-12-19 16:44:52 +0800

    完善工具类的错误，登录页面成功运行至管理页面

--- src/main/java/com/tt/teach/controller/StudentController.java
+++ src/main/java/com/tt/teach/controller/StudentController.java
@@ -6,9 +6,7 @@ import com.tt.teach.utils.BaseController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
 import javax.annotation.Resource;
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 
--- src/main/java/com/tt/teach/utils/BaseController.java
+++ src/main/java/com/tt/teach/utils/BaseController.java
@@ -22,8 +22,8 @@ public class BaseController {
     private static final Logger logger= LoggerFactory.getLogger(BaseController.class);
 //    2.常用的变量提取出来，放到Base中方便使用
     protected static final String SESSION_KEY="studentName";
-    protected static final String FORWARD ="forward";
-    protected static final String REDIRECT="redirece";
+    protected static final String FORWARD ="forward:";
+    protected static final String REDIRECT="redirect:";
 
 //    3.常用request、session、response的获取
 
